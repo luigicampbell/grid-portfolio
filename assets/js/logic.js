85a37c0531dc1dca504aa689348c603fa5457c54
BYRON CAMPBELL <blcampbell@alumni.cpp.edu>
2018-01-31 13:09:55 -0800

    working on navbar strike through. Finished hamburger button

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,7 +1,15 @@
+let wrapperMenu = document.querySelector('.wrapper-menu');
 function openNav() {
     document.getElementById("navbar").style.height = "100%";
+    wrapperMenu.classList.toggle('open');
 }
 
 function closeNav() {
     document.getElementById("navbar").style.height = "0%";
+    wrapperMenu.classList.toggle('open');
 }
+// var wrapperMenu = document.querySelector('.wrapper-menu');
+//
+// wrapperMenu.addEventListener('click', function(){
+//   wrapperMenu.classList.toggle('open');
+// })
