c6cd01bad1dc6d7fe726cb79c3f2b6bfcfd44c1e
BYRON CAMPBELL <blcampbell@alumni.cpp.edu>
2018-02-02 14:21:57 -0800

    added other pages, working on burger transform

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -8,8 +8,3 @@ function closeNav() {
     document.getElementById("navbar").style.height = "0%";
     wrapperMenu.classList.toggle('open');
 }
-// var wrapperMenu = document.querySelector('.wrapper-menu');
-//
-// wrapperMenu.addEventListener('click', function(){
-//   wrapperMenu.classList.toggle('open');
-// })
